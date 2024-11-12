091b98b797891cf3d6cc77d87fd9911e1c5c0088
SindreSau <sindre_50@hotmail.com>
2024-11-12 13:18:23 +0100

    Added output: standalone for aws lambda

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  output: "standalone", // Important for AWS Lambda
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname, "src");
     return config;
