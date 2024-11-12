bbb965cf1f3376c4e245ff9bd726803680990ef4
SindreSau <sindre_50@hotmail.com>
2024-11-12 12:56:07 +0100

    feat: add console log of DATABASE_URL

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
+console.log("Using DATABASE_URL", process.env.DATABASE_URL);
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl:
